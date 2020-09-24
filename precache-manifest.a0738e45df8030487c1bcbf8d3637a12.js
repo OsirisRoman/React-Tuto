4844d522e5941e048bb8938c4c79b87d92581bc2
Osiris Román <osirisr1994@gmail.com>
2020-09-24 13:18:42 -0500

    Updates

---
+++ precache-manifest.a0738e45df8030487c1bcbf8d3637a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc4afc0caeb6a675cfb225574d792b4",
+    "url": "/React-Tuto/index.html"
+  },
+  {
+    "revision": "43c0e865be7fec4dc123",
+    "url": "/React-Tuto/static/css/main.4af1cc4e.chunk.css"
+  },
+  {
+    "revision": "ad3d9b6d3705bae367b0",
+    "url": "/React-Tuto/static/js/2.a4774fd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tuto/static/js/2.a4774fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c0e865be7fec4dc123",
+    "url": "/React-Tuto/static/js/main.b1d72616.chunk.js"
+  },
+  {
+    "revision": "8eefde8d71f475a2d500",
+    "url": "/React-Tuto/static/js/runtime-main.dcfe0107.js"
+  }
+]);
