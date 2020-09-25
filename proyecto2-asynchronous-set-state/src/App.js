47e18fe6d41c9e59222942a0f1870029c2f0a95d
Osiris Román <osirisr1994@gmail.com>
2020-09-24 21:57:04 -0500

    Alternate Class Syntax
    El estado local se puede inicializar sin necesidad del constructor como una declaración de campo de clase para intentar ser más conciso. Hay que recordar que los métodos se deben declarar como funciones flecha para evitar el bind extra.

--- proyecto2-asynchronous-set-state/src/App.js
+++ proyecto2-asynchronous-set-state/src/App.js
@@ -3,16 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      meaningOfLife: 47 + this.props.increment
+  state = {
+      meaningOfLife: 47
     }
-  }
 
   handleClick = () => {
     this.setState((prevState, prevProp) => {
-      return {meaningOfLife: prevState.meaningOfLife + prevProp.increment}
+      return {meaningOfLife: prevState.meaningOfLife + 1}
     })
   }
   render() {
